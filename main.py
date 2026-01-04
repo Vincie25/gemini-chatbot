60369dd6a6a14e3c35fb9b6f0c250b2ca571c2b6
Vincie25 <googlfreestyle@gmail.com>
2026-01-05 00:28:20 +0100

    creating loop agent and finishing functionality of the chatbot

--- main.py
+++ main.py
@@ -19,32 +19,42 @@ def main() ->None:
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(
-            model='gemini-2.5-flash', contents=messages,
-            config=types.GenerateContentConfig(tools=[available_functions], 
-                                               system_instruction=system_prompt)
-            )
-    if response.usage_metadata is None:
-        raise RuntimeError("failed API request")
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    if response.function_calls is None:
-        print(response.text)
-    else:
-        function_results = []
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call, verbose=args.verbose)
-            if not function_call_result.parts:
-                raise Exception("Error")
-            if not function_call_result.parts[0].function_response:
-                raise Exception("Error")
-            if not function_call_result.parts[0].function_response.response:
-                raise Exception("Error")
-            function_results.append(function_call_result.parts[0])
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
+    for _ in range(20):
+        response = client.models.generate_content(
+            model='gemini-3-flash', contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions],
+                                               system_instruction=system_prompt)
+            )
+        if response.usage_metadata is None:
+            raise RuntimeError("failed API request")
+        if response.candidates:
+            for candidate in response.candidates:
+                if candidate.content:
+                    messages.append(candidate.content)
+        if args.verbose:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if not response.function_calls:
+            print(response.text)
+            return
+        else:
+            function_results = []
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call, verbose=args.verbose)
+                if not function_call_result.parts:
+                    raise Exception("Error")
+                if not function_call_result.parts[0].function_response:
+                    raise Exception("Error")
+                if not function_call_result.parts[0].function_response.response:
+                    raise Exception("Error")
+                function_results.append(function_call_result.parts[0])
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+            messages.append(types.Content(role="user", parts=function_results))
+    print("Maximum iterations reached without final response")
+    exit(1)
 
 
 if __name__ == "__main__":
